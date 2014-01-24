9d3e1a5a7d1514677743458bb61a3c2743f2d81f
Jean-André Santoni <jean.andre.santoni@gmail.com>
2014-01-24 14:46:00 +0100

    Initial Commit

---
+++ Rouen2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Rouen2
+//
+//  Created by Jean-André Santoni on 24/01/14.
+//  Copyright (c) 2014 Jean-André Santoni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Rouen2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Rouen2
+//
+//  Created by Jean-André Santoni on 24/01/14.
+//  Copyright (c) 2014 Jean-André Santoni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
