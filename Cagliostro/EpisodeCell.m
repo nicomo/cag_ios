2feff99c4b89ed023667bb04e450302b74d24f09
Jean-André Santoni <jean.andre.santoni@gmail.com>
2014-03-12 12:49:44 +0100

    Two lines for episode labels on the homepage

--- Cagliostro/EpisodeCell.m
+++ Cagliostro/EpisodeCell.m
@@ -16,17 +16,22 @@
     {
         self.backgroundColor = [UIColor yellowColor];
         
-        UIView *bottom = [[UIView alloc] initWithFrame:CGRectMake(0, frame.size.height - 30, frame.size.width, 30)];
+        UIView *bottom = [[UIView alloc] initWithFrame:CGRectMake(0, frame.size.height - 40, frame.size.width, 40)];
         bottom.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:0.75];
 
-        
-        self.title = [[UILabel alloc] initWithFrame:CGRectMake(5, 0, frame.size.width-10, 30)];
+        self.title = [[UILabel alloc] initWithFrame:CGRectMake(5, 0, frame.size.width-10, 40)];
         self.title.font = [UIFont fontWithName:@"SuperClarendon-Black" size:10];
         self.title.textColor = [UIColor colorWithRed:0.24 green:0.20 blue:0.12 alpha:1.0];
         self.title.userInteractionEnabled = NO;
+        self.title.numberOfLines = 2;
         
-        self.thumb = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"homecard1"]];
-        self.thumb.frame = CGRectMake(0, 0, frame.size.width, frame.size.height);
+        if (frame.size.height == 130) {
+            self.thumb = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"homecard2"]];
+            self.thumb.frame = CGRectMake(0, 0, frame.size.width, frame.size.height);
+        } else {
+            self.thumb = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"homecard1"]];
+            self.thumb.frame = CGRectMake(0, 0, frame.size.width, frame.size.height);
+        }
         
         self.layer.borderColor = [UIColor colorWithRed:.75 green:.70 blue:.69 alpha:1.0].CGColor;
         self.layer.borderWidth = 1;
