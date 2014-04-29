bd5011dd9d8eb3cf6aafd8140f793f9125a2bfab
Jean-André Santoni <jean.andre.santoni@gmail.com>
2014-04-29 17:22:48 +0700

    Character page header

--- Cagliostro/CharacterViewController.m
+++ Cagliostro/CharacterViewController.m
@@ -8,10 +8,13 @@
 
 #import "AppDelegate.h"
 #import "CharacterViewController.h"
+#import "CSLinearLayoutView.h"
 
 @interface CharacterViewController ()
 
 @end
+
+CSLinearLayoutView *mainLinearLayout;
 
 @implementation CharacterViewController
 
@@ -37,6 +40,40 @@
     self.view.backgroundColor = [UIColor colorWithRed:0.96 green:0.94 blue:0.89 alpha:1.0];
     
     self.navigationItem.title = [chardata[self.cid] objectForKey:@"name"];
+    
+    // create the linear layout view
+    mainLinearLayout = [[CSLinearLayoutView alloc] initWithFrame:self.view.bounds];
+    mainLinearLayout.backgroundColor = [UIColor clearColor];
+    mainLinearLayout.orientation = CSLinearLayoutViewOrientationVertical;
+    [self.view addSubview:mainLinearLayout];
+    
+    [self addHeader];
+}
+
+- (void)addHeader
+{
+    UIImageView *header = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 768, 400)];
+    [header setImage:[UIImage imageNamed:@"charbg"]];
+    
+    UIView *whitestripe = [[UIView alloc] initWithFrame:CGRectMake(0, 200, 768, 200)];
+    [whitestripe setBackgroundColor:[UIColor whiteColor]];
+    [whitestripe setAlpha:0.5];
+    [header addSubview:whitestripe];
+    
+    UIImageView *avatar = [[UIImageView alloc] initWithFrame:CGRectMake(50, 250, 100, 100)];
+    [avatar setImage:[UIImage imageNamed:[NSString stringWithFormat:@"character%d", self.cid]]];
+    [header addSubview:avatar];
+    
+    UILabel *name = [[UILabel alloc] initWithFrame:CGRectMake(50+100+50, 250, 400, 100)];
+    [name setText:[chardata[self.cid] objectForKey:@"name"]];
+    [name setFont:[UIFont fontWithName:@"SuperClarendon-Black" size:30]];
+    name.textColor = [UIColor colorWithRed:0.24 green:0.20 blue:0.12 alpha:1.0];
+    name.userInteractionEnabled = NO;
+    name.numberOfLines = 2;
+    [header addSubview:name];
+    
+    CSLinearLayoutItem *headeritem = [CSLinearLayoutItem layoutItemForView:header];
+    [mainLinearLayout addItem:headeritem];
 }
 
 - (void)didReceiveMemoryWarning
