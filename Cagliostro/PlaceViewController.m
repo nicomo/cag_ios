6cfd29259f2c45b23696b938e3781787c2b45d69
Jean-André Santoni <jean.andre.santoni@gmail.com>
2014-05-25 15:08:34 +0200

    Places, insert upper textzones

--- Cagliostro/PlaceViewController.m
+++ Cagliostro/PlaceViewController.m
@@ -85,6 +85,93 @@ CSLinearLayoutView *mainLinearLayout;
     headeritem.padding = CSLinearLayoutMakePadding(0, 50, 0, 50);
     [mainLinearLayout addItem:headeritem];
     
+    // First textzone
+    
+    CSLinearLayoutView *ll1 = [[CSLinearLayoutView alloc] initWithFrame:CGRectMake(0, 0, 668, 100)];
+    ll1.autoresizesSubviews = YES;
+    ll1.orientation = CSLinearLayoutViewOrientationHorizontal;
+    
+    UIImageView *bullhorn = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 50, 50)];
+    [bullhorn setImage:[UIImage imageNamed:@"bullhorn"]];
+    CSLinearLayoutItem *bhitem = [CSLinearLayoutItem layoutItemForView:bullhorn];
+    [ll1 addItem:bhitem];
+    
+    UITextView *intro = [[UITextView alloc] initWithFrame:CGRectMake(0, 0, 309, 100)];
+    [intro setText:[pldata[self.plid] objectForKey:@"intro"]];
+    [intro setFont:[UIFont fontWithName:@"Georgia" size:12]];
+    intro.backgroundColor = [UIColor clearColor];
+    intro.textContainerInset = UIEdgeInsetsMake(10,10,10,10);
+    [intro sizeToFit];
+    CSLinearLayoutItem *introitem = [CSLinearLayoutItem layoutItemForView:intro];
+    [ll1 addItem:introitem];
+    
+    CSLinearLayoutView *llv1 = [[CSLinearLayoutView alloc] initWithFrame:CGRectMake(0, 0, 309, 0)];
+    llv1.autoAdjustFrameSize = YES;
+    llv1.orientation = CSLinearLayoutViewOrientationVertical;
+    
+    for (int i = 0; i < 3; i++) {
+        
+        CSLinearLayoutView *lli1 = [[CSLinearLayoutView alloc] initWithFrame:CGRectMake(0, 0, 309, 40)];
+        lli1.autoAdjustFrameSize = YES;
+        lli1.orientation = CSLinearLayoutViewOrientationHorizontal;
+        
+        UIImageView *iticon1 = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 40, 40)];
+        [iticon1 setImage:[UIImage imageNamed:[NSString stringWithFormat:@"itin_%d", i]]];
+        CSLinearLayoutItem *iticon1item = [CSLinearLayoutItem layoutItemForView:iticon1];
+        [lli1 addItem:iticon1item];
+        
+        UITextView *itin1 = [[UITextView alloc] initWithFrame:CGRectMake(0, 0, 309-20-40, 0)];
+        itin1.text = [[pldata[self.plid] objectForKey:@"itineraire"] objectAtIndex:i];
+        itin1.font = [UIFont fontWithName:@"Georgia" size:12];
+        itin1.backgroundColor = [UIColor clearColor];
+        itin1.textContainerInset = UIEdgeInsetsMake(5,5,5,5);
+        [itin1 sizeToFit];
+        itin1.frame = CGRectMake(0, 0, 309-20-40, itin1.frame.size.height);
+        CSLinearLayoutItem *itin1item = [CSLinearLayoutItem layoutItemForView:itin1];
+        [lli1 addItem:itin1item];
+        
+        CSLinearLayoutItem *lli1item = [CSLinearLayoutItem layoutItemForView:lli1];
+        lli1item.padding = CSLinearLayoutMakePadding(10, 10, 0, 10);
+        [llv1 addItem:lli1item];
+    }
+    
+    UITextView *histotitle = [[UITextView alloc] initWithFrame:CGRectMake(0, 0, 309, 0)];
+    histotitle.text = @"HISTORIQUE";
+    histotitle.font = [UIFont fontWithName:@"Georgia" size:12];
+    histotitle.backgroundColor = [UIColor clearColor];
+    histotitle.textContainerInset = UIEdgeInsetsMake(10,10,10,10);
+    histotitle.layer.borderColor = [UIColor colorWithRed:.75 green:.70 blue:.69 alpha:1.0].CGColor;
+    histotitle.layer.borderWidth = 1;
+    [histotitle sizeToFit];
+    histotitle.frame = CGRectMake(0, 0, 309, histotitle.frame.size.height);
+    CSLinearLayoutItem *htitem = [CSLinearLayoutItem layoutItemForView:histotitle];
+    htitem.padding = CSLinearLayoutMakePadding(15, 0, 0, 0);
+    [llv1 addItem:htitem];
+    
+    UITextView *historique = [[UITextView alloc] initWithFrame:CGRectMake(0, 0, 309, 0)];
+    historique.text = [pldata[self.plid] objectForKey:@"historique"];
+    historique.font = [UIFont fontWithName:@"Georgia" size:12];
+    historique.backgroundColor = [UIColor clearColor];
+    historique.textContainerInset = UIEdgeInsetsMake(10,10,10,10);
+    historique.layer.borderColor = [UIColor colorWithRed:.75 green:.70 blue:.69 alpha:1.0].CGColor;
+    historique.layer.borderWidth = 1;
+    [historique sizeToFit];
+    historique.frame = CGRectMake(0, 0, 309, historique.frame.size.height);
+    CSLinearLayoutItem *hitem = [CSLinearLayoutItem layoutItemForView:historique];
+    [llv1 addItem:hitem];
+    
+    CSLinearLayoutItem *llv1item = [CSLinearLayoutItem layoutItemForView:llv1];
+    [ll1 addItem:llv1item];
+    
+    if (intro.frame.size.height > ll1.frame.size.height)
+        ll1.frame = CGRectMake(0, 0, 668, intro.frame.size.height);
+    if (llv1.frame.size.height > ll1.frame.size.height)
+        ll1.frame = CGRectMake(0, 0, 668, llv1.frame.size.height);
+    
+    CSLinearLayoutItem *ll1item = [CSLinearLayoutItem layoutItemForView:ll1];
+    ll1item.padding = CSLinearLayoutMakePadding(0, 50, 50, 50);
+    [mainLinearLayout addItem:ll1item];
+    
     // Map
     
     self.map = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 668, 476)];
